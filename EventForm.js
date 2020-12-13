a6eba3298c83f46c8578790303adb3ba9b90e0b8
Dennis Sanders <isidennissanders@hotmail.com>
2020-12-13 12:53:57 -0600

    Add changes through Module 6.1

--- EventForm.js
+++ EventForm.js
@@ -1,14 +1,46 @@
 import React, { Component } from "react";
-import { View, Text, TouchableHighlight } from "react-native";
+import {
+  View,
+  Text,
+  TouchableHighlight,
+  TextInput,
+  StyleSheet,
+} from "react-native";
+
+const styles = StyleSheet.create({
+  fieldContainer: {
+    marginTop: 20,
+    marginBottom: 20,
+    backgroundColor: "#fff",
+  },
+  text: {
+    height: 40,
+    margin: 0,
+    marginRight: 7,
+    paddingLeft: 10,
+  },
+});
 
 class EventForm extends Component {
+  state = {
+    title: null,
+    date: "",
+  };
+
   handleAddPress = () => {
     this.props.navigation.navigate("List");
   };
 
   render() {
     return (
-      <View>
+      <View style={{ flex: 1 }}>
+        <View style={styles.fieldContainer}>
+          <TextInput
+            style={styles.text}
+            placeholder="Event title"
+            spellCheck={false}
+          />
+        </View>
         <TouchableHighlight onPress={this.handleAddPress}>
           <Text>Add</Text>
         </TouchableHighlight>
