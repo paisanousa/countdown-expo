03b2ed33ea129ba076f121ab6c7994fa08fbefdb
Dennis Sanders <isidennissanders@hotmail.com>
2020-12-12 21:02:19 -0600

    Add changes up to 4.28 of Module 4.3

---
+++ EventCard.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import PropTypes from "prop-types";
+import { formatDate } from "./api";
+
+const styles = StyleSheet.create({});
+
+export default function EventCard({ event }) {
+  return (
+    <View style={styles.card}>
+      <View style={styles.cardHeader}>
+        <Text style={styles.date}>{formatDate(event.date)}</Text>
+        <Text style={styles.title}>{event.title}</Text>
+      </View>
+    </View>
+  );
+}
+
+EventCard.propTypes = {
+  event: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+  }),
+};
--- EventList.js
+++ EventList.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import { FlatList, Text } from "react-native";
+import { FlatList } from "react-native";
+
+import EventCard from "./EventCard";
 
 class EventList extends Component {
   state = {
@@ -15,7 +17,7 @@ class EventList extends Component {
     return (
       <FlatList
         data={this.state.events}
-        renderItem={({ item }) => <Text>{item.title}</Text>}
+        renderItem={({ item }) => <EventCard event={item} />}
         keyExtractor={(item) => item.id}
       />
     );
