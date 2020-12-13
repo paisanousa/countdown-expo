4ed6dd5d40e4f23a2a9d7d50db5a2e69dbeca41f
Dennis Sanders <isidennissanders@hotmail.com>
2020-12-12 20:01:51 -0600

    Add changes through Module 4.1

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
 import { LogBox } from "react-native";
+import EventList from "./EventList";
 
 LogBox.ignoreLogs([
   "Warning: componentWillMount is deprecated",
@@ -9,20 +9,6 @@ LogBox.ignoreLogs([
 
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hello</Text>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+    return <EventList />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
